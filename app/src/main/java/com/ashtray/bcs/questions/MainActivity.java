8e9c7e039cb761004f83789a0978833b10b1d15d
ashtray4715 <52469753+ashtray4715@users.noreply.github.com>
2020-05-05 03:32:56 +0600

    Adding web view in main activity

--- app/src/main/java/com/ashtray/bcs/questions/MainActivity.java
+++ app/src/main/java/com/ashtray/bcs/questions/MainActivity.java
@@ -2,13 +2,33 @@ package com.ashtray.bcs.questions;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        webView.getSettings().setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+            return;
+        }
+        super.onBackPressed();
     }
 }
